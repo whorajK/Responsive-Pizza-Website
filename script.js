4e7f33ccadca8ddfb88aa2d079083eca07c88a71
Raj Changappa <102789104+whorajK@users.noreply.github.com>
2023-01-16 15:02:35 +0530

    footer added

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-active");
   document.querySelector(".nav--menu").classList.toggle("is-active");
 });
+
+const links = document.querySelectorAll(".nav--link");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.querySelector(".nav--menu").classList.remove("is-active");
+    hamburger.classList.remove("is-active");
+  });
+});
